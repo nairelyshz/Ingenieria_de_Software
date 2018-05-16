ae83f53054634a017c25d11e54606dc4ccb618f9
Nairelys Hernandez <njhs694@gmail.com>
2018-05-16 18:25:35 -0400

    se gregaron las pruebas para la cantidad letras y digitos validos de una contraseña

--- tarea3/apps/usuario/tests.py
+++ tarea3/apps/usuario/tests.py
@@ -10,11 +10,19 @@ class TestSeguridad(TestCase):
 
 	def test_registro_clave_mayor_16(self):
 		seg = Seguridad()
-		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","1234567maria++77.","1234567maria++77."))
+		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","1234567maria++77","1234567maria++77"))
 
 	def test_registro_min_tres_letras(self):
 		seg = Seguridad()
-		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","12345678.","12345678."))
+		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","12345678","12345678"))
+
+	def test_registro_min_tres_letras_una_mayus(self):
+		seg = Seguridad()
+		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","123mar45678","123mars45678"))
+
+	def test_registro_password_una_mayus_un_dig_tres_letras(self):
+		seg = Seguridad()
+		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","1232mAr45678","1232mAr45678"))
 
 	def test_Seguridad_login(self):
 		seg = Seguridad()
