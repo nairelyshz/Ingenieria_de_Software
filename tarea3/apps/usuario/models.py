88d9838246593b28d8b9bff3d5978e07e4879013
Nairelys Hernandez <njhs694@gmail.com>
2018-05-17 01:07:44 -0400

    Atualizacion de pruebas

--- tarea3/apps/usuario/models.py
+++ tarea3/apps/usuario/models.py
@@ -4,22 +4,50 @@ from django.db import models
 
 class Seguridad(models.Model):
 	"""docstring for Seguridad"""
-
+	users = {}
 
 	eMail = models.EmailField()
 	password1 = models.CharField(max_length=16)
 	password2 = models.CharField(max_length=16)
 
-	def showData():
-		print (eMail)
-		pass
+
 
 	def registrarUsuario(self,id,password1,password2):
-		if (len(password1)>=8 and len(password1)<=16) and (password1==password2):
-			
-			return "Usuario válido"
-		else: 
-			return "Clave invalida"
+		#Primer argumento debe ser de tipo string
+		if not isinstance(id, str):
+			raise TypeError ("ERROR: Primer argumento debe ser de tipo str")
+    #Segundo argumento debe ser de tipo string
+		if not isinstance(password1, str):
+			raise TypeError ("ERROR: Segundo argumento debe ser de tipo lista")
+    #Tercer argumento debe ser de tipo string
+		if not isinstance(password2, str):
+			raise TypeError ("ERROR: Tercer argumento debe ser de tipo lista")
+		valido = False;
+		contadorArroba = 0;
+		contadorPunto = 0;
+		for i in id:
+			if(i == '@'):
+				contadorArroba += 1
+				valido = True
+			if(contadorArroba != 1 and contadorPunto < 1):
+				valido = False
+			if(i == '.'):
+				contadorPunto += 1
+		if(valido):
+			if (len(password1)>=8 and len(password1)<=16) and (password1==password2):
+				self.users.setdefault(id,password1)
+				return "Usuario válido"
+			else: 
+				return "Clave invalida"
+		else:
+   			return "direccion erronea"
+		
 
 	def IngresarUsuario(self,id, pass1):
-		pass
+		if self.users.get(id)!=None:
+			if self.users.get(id)==pass1 :
+				return "Usuario aceptado"
+			else:
+				return "Clave inválida"
+		else:
+			return "Usuario inválido"
--- tarea3/apps/usuario/tests.py
+++ tarea3/apps/usuario/tests.py
@@ -3,8 +3,20 @@ from apps.usuario.models import Seguridad
 # Create your tests here.
 class TestSeguridad(TestCase):
 	
-	def test_registro_clave_menor_8(self):
+	def test_registrar_primer_argumento(self):
+		seg = Seguridad()
+		self.assertRaises(TypeError, registrarUsuario, Decimal(2), "hola_1", "hola_2")
+		self.assertRaises(TypeError, registrarUsuario, [], "hola_1", "hola_2")
+		regisro = seg.registrarUsuario("hola_1", "hola_2", "hola_3") 
+	
+	def test_registrar_segundo_argumento(self):
+		seg = Seguridad()
+		self.assertRaises(TypeError, registrarUsuario, Decimal(1), "hola")
+		self.assertRaises(TypeError, registrarUsuario, Decimal(1), [])
+		registro = seg.registrarUsuario("id", "pass1","pass2")
 
+
+	def test_registro_clave_menor_8(self):
 		seg = Seguridad()
 		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","1234567","1234567"))
 
@@ -14,7 +26,8 @@ class TestSeguridad(TestCase):
 
 	def test_registro_min_tres_letras(self):
 		seg = Seguridad()
-		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","12345678","12345678"))
+		self.assertRaises(TypeError,registrarUsuario,"12-10199@gmail.com","12345678","12345678")
+		regisro = registrarUsuario("12-10199@gmail.com","1234mns5678","12345678")
 
 	def test_registro_min_tres_letras_una_mayus(self):
 		seg = Seguridad()
@@ -22,10 +35,9 @@ class TestSeguridad(TestCase):
 
 	def test_registro_password_una_mayus_un_dig_tres_letras(self):
 		seg = Seguridad()
-		self.assertEqual("Clave invalida",seg.registrarUsuario("12-10199@gmail.com","1232mAr45678","1232mAr45678"))
+		self.assertEqual("Usuario válido",seg.registrarUsuario("12-10199@gmail.com","1232mAr45678","1232mAr45678"))
 
-	def test_Seguridad_login(self):
-		seg = Seguridad()
-		self.assertEqual(None,seg.IngresarUsuario("12-10199@usb.ve",12345678))
+	
+	
 
 	
